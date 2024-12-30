d3ee68403f59224612b1355492d9fe5d4c4c4cc2
SATA1251 <goldpig622@naver.com>
2024-12-30 16:16:47 +0900

    블록 파괴시 젬 획득 초안 커밋

--- ProjectMine/Assets/Script/Map/WallScript.cs
+++ ProjectMine/Assets/Script/Map/WallScript.cs
@@ -8,20 +8,31 @@ public class WallScript : PoolAble
     [SerializeField]
     public GameObject planeData;
 
+    public GameObject _player;
+
     public GameObject _hitParticleObject;
 
     public Transform _hitParticleTransform;
 
     public IObjectPool<GameObject> Pool { get; set; }
 
+    private float _dropAmountS;
+    private float _dropAmountA;
+    private float _dropAmountB;
+
     public void Start()
     {
         _hitParticleObject = GameObject.Find("BlockHit");
+        _player = GameObject.Find("Player");
+        _dropAmountB = 5f;
+        _dropAmountS = 10f;
+        _dropAmountA = 20f;
     }
     public void DestroyWall()
     {
         ReleaseObject();
         SpawnParticle();
+        DropAmount();
         //Destroy(gameObject);        
         //DestroyWallLine(0,0);
     }
@@ -44,6 +55,24 @@ public class WallScript : PoolAble
             GameObject particle = Instantiate(_hitParticleObject, position, rotation);
 
             Destroy(particle, particle.GetComponent<ParticleSystem>().main.duration);
+        }
+    }
+
+    private void DropAmount()
+    {
+        float randomValue = Random.Range(0f, 1000f); 
+
+        if(randomValue <= _dropAmountS)
+        {
+            _player.GetComponent<PlayerController>().oresSamount++;
+        }
+        else if(randomValue <= _dropAmountA)
+        {
+            _player.GetComponent<PlayerController>().oresAamount++;
+        }
+        else if (randomValue <= _dropAmountB)
+        {
+            _player.GetComponent<PlayerController>().oresBamount++;
         }
     }
 }
